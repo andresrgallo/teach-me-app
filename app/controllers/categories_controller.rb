60c505d454d0f0739db0681562f4a5d6c7e8dec9
andresrgallo <andresgallo@hotmail.com>
2018-05-03 16:54:11 +1000

    Add Category Model, Controller and all views

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,65 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+  def index
+    @categories = Category.all
+  end
+
+  def show
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def edit
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    respond_to do |format|
+      if @category.save
+
+        # if params[:images]
+        #   params[:images].each { |image|
+        #     @category.images_categories.create(image: image)
+        #   }
+        # end
+
+        format.html { redirect_to @category, notice: 'category was successfully created.' }
+        format.json { render :show, status: :created, location: @category }
+      else
+        format.html { render :new }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @category.update(category_params)
+        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
+        format.json { render :show, status: :ok, location: @listing }
+      else
+        format.html { render :edit }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def category_params
+    params.require(:category).permit(:description)
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :categories
+
   devise_for :users
-  root 'home#index'
+  root 'categories#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
