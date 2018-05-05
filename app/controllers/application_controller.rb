b87f82f1d7858b736413fa8e05e45adf2358dcf4
andresrgallo <andresgallo@hotmail.com>
2018-05-05 16:43:09 +1000

    Fix profile link at Home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   # Reditect to a default route when user inputs a wrong one
-  rescue_from ActionController::RoutingError do |exception|
-    flash[:error] = "There is no such route"
-    redirect_to root_url
-  end
+  # rescue_from ActionController::RoutingError do |exception|
+  #   flash[:error] = "There is no such route"
+  #   redirect_to root_url
+  # end
 
   private
 
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource_or_scope)
 
       if Profile.exists?(:user_id => current_user.id)
-        profile_path(current_user.id)
+        profile_path(current_user.profile.id)
       else
         new_profile_path
       end
