81f22706ef2fd01bfee409d8ebe4912f26871225
andresrgallo <andresgallo@hotmail.com>
2018-05-10 21:45:14 +1000

    Add styling

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,7 +7,7 @@ class Category < ApplicationRecord
     has_attached_file :image, styles: {
         thumb: '100x100>',
         square: '200x200#',
-        medium: '300x300>'
+        medium: '300x300>',
       }
     
       # Validate the attached image is image/jpg, image/png, etc
